a4bac25a534ed89e56a182db2c37092725f35c4f
Yaser Sharifi <yaser@raadco.com.au>
2025-03-16 13:51:48 +0330

    feat: implement real time pose detection

---
+++ real_time_pose_detection/android/app/src/main/kotlin/com/example/real/time/pose/detection/real_time_pose_detection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.real.time.pose.detection.real_time_pose_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ real_time_pose_detection/lib/main.dart
@@ -0,0 +1,287 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(
+        title: 'screen',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  dynamic controller;
+  bool isBusy = false;
+  late Size size;
+
+  //TODO declare detector
+  late PoseDetector poseDetector;
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+  }
+
+  //TODO code to initialize the camera feed
+  initializeCamera() async {
+    //TODO initialize detector
+    final options = PoseDetectorOptions(
+        mode: PoseDetectionMode.stream);
+    poseDetector = PoseDetector(options: options);
+
+    controller = CameraController(cameras[0], ResolutionPreset.medium,imageFormatGroup: Platform.isAndroid
+        ? ImageFormatGroup.nv21
+        : ImageFormatGroup.bgra8888,  );
+    await controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      controller.startImageStream((image) => {
+        if (!isBusy)
+          {isBusy = true, img = image, doPoseEstimationOnFrame()}
+      });
+    });
+  }
+
+  //TODO pose detection on a frame
+  dynamic _scanResults;
+  CameraImage? img;
+  doPoseEstimationOnFrame() async {
+    var inputImage = _inputImageFromCameraImage();
+    if (inputImage !=null) {
+      final List<Pose> poses = await poseDetector.processImage(inputImage!);
+      _scanResults = poses;
+
+    }
+
+    setState(() {
+      _scanResults;
+      isBusy = false;
+    });
+  }
+
+  //close all resources
+  @override
+  void dispose() {
+    controller?.dispose();
+    poseDetector.close();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> stackChildren = [];
+    size = MediaQuery.of(context).size;
+    if (controller != null) {
+      stackChildren.add(
+        Positioned(
+          top: 0.0,
+          left: 0.0,
+          width: size.width,
+          height: size.height,
+          child: Container(
+            child: (controller.value.isInitialized)
+                ? AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: CameraPreview(controller),
+            )
+                : Container(),
+          ),
+        ),
+      );
+
+      stackChildren.add(
+        Positioned(
+            top: 0.0,
+            left: 0.0,
+            width: size.width,
+            height: size.height,
+            child: buildResult()),
+      );
+    }
+
+    return Scaffold(
+
+      body: Container(
+          margin: const EdgeInsets.only(top: 0),
+          color: Colors.black,
+          child: Stack(
+            children: stackChildren,
+          )),
+    );
+  }
+
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+  InputImage? _inputImageFromCameraImage() {
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas
+    final camera = cameras[0];
+    final sensorOrientation = camera.sensorOrientation;
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+      _orientations[controller!.value.deviceOrientation];
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+    }
+    if (rotation == null) return null;
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(img!.format.raw);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (img!.planes.length != 1) return null;
+    final plane = img!.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(img!.width.toDouble(), img!.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
+  }
+
+  //Show rectangles around detected objects
+  Widget buildResult() {
+    if (_scanResults == null ||
+        controller == null ||
+        !controller.value.isInitialized) {
+      return Text('');
+    }
+    final Size imageSize = Size(
+      controller.value.previewSize!.height,
+      controller.value.previewSize!.width,
+    );
+    CustomPainter painter = PosePainter(imageSize, _scanResults);
+    return CustomPaint(
+      painter: painter,
+    );
+  }
+}
+
+class PosePainter extends CustomPainter {
+  PosePainter(this.absoluteImageSize, this.poses);
+
+  final Size absoluteImageSize;
+  final List<Pose> poses;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0
+      ..color = Colors.green;
+
+    final leftPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.yellow;
+
+    final rightPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.blueAccent;
+
+    for (final pose in poses) {
+      pose.landmarks.forEach((_, landmark) {
+        canvas.drawCircle(
+            Offset(landmark.x * scaleX, landmark.y * scaleY), 1, paint);
+      });
+
+      void paintLine(
+          PoseLandmarkType type1, PoseLandmarkType type2, Paint paintType) {
+        final PoseLandmark joint1 = pose.landmarks[type1]!;
+        final PoseLandmark joint2 = pose.landmarks[type2]!;
+        canvas.drawLine(Offset(joint1.x * scaleX, joint1.y * scaleY),
+            Offset(joint2.x * scaleX, joint2.y * scaleY), paintType);
+      }
+
+      // //Draw arms
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow, leftPaint);
+      paintLine(
+          PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow,
+          rightPaint);
+      paintLine(
+          PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist, rightPaint);
+
+      //Draw Body
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip,
+          rightPaint);
+
+      //Draw legs
+      paintLine(PoseLandmarkType.leftHip, PoseLandmarkType.leftKnee, leftPaint);
+      paintLine(
+          PoseLandmarkType.leftKnee, PoseLandmarkType.leftAnkle, leftPaint);
+      paintLine(
+          PoseLandmarkType.rightHip, PoseLandmarkType.rightKnee, rightPaint);
+      paintLine(
+          PoseLandmarkType.rightKnee, PoseLandmarkType.rightAnkle, rightPaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(PosePainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.poses != poses;
+  }
+}
